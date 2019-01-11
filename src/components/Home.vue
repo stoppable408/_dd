e418e27aba529cf49b808dee58a8ada2729f03d9
Lennon Turner <lennonturner92@gmail.com>
2019-01-11 16:48:44 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -342,21 +342,7 @@
               </div>
              </div>
              
-           </div>
-            <div class="card">
-              <div class="faq-header card-header" id="headingEight" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseThree">
-                <h5 class="mb-0">
-                  <p class="question">
-                  Can I bring snacks to share?              
-                  </p>
-                </h5>
-              </div>
-              <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
-                <div class="card-body answer">
-                  YES PLEASE.  ALWAYS.  (Ask about allergies though just in case.)
-                </div>
-              </div>
-             </div>
+ 
             
           </div>
         </div>
