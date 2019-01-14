c23714287a1d6347e16f97bce982e63a511c60e9
Lennon Turner <lennonturner92@gmail.com>
2019-01-14 13:53:48 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -204,6 +204,9 @@
               <div class="carousel-item active">
                 <img class="d-block w-100 gallery-img" src="../assets/D&D1.jpg" alt="First slide">
               </div>
+              <div class="carousel-item">
+                <img class="d-block w-100 gallery-img" src="../assets/Doug and Steve.jpg" alt="Second slide">
+              </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
