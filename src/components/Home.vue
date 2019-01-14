655a7b0a3c2cfa1a3a6bf4d9c43e31ce153140ad
Lennon Turner <lennonturner92@gmail.com>
2019-01-14 13:38:58 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -202,7 +202,7 @@
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img class="d-block w-100 gallery-img" src="../assets/D&D1.jpg" alt="First slide">
+                <img class="d-block w-100 gallery-img" src="../assets/D&D 1.jpg" alt="First slide">
               </div>
               <div class="carousel-item">
                 <img class="d-block w-100 gallery-img" src="../assets/Doug and Steve.jpg" alt="Second slide">
